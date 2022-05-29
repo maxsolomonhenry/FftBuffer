b955c583fab0c9bc2013068d2b9304b1e1f94da1
Max Henry <max.henry@mail.mcgill.ca>
2022-05-29 15:10:54 -0400

    Build up cpp base

---
+++ FftBuffer/Source/OlaBuffer.cpp
@@ -0,0 +1,6 @@
+//
+//  OlaBuffer.cpp
+//  Created by Max Henry on 2022-05-29.
+//
+
+#include "OlaBuffer.h"
---
+++ FftBuffer/Source/OlaBuffer.h
@@ -0,0 +1,13 @@
+//
+//  OlaBuffer.h
+//  Created by Max Henry on 2022-05-29.
+//
+
+#pragma once
+
+class OlaBuffer
+{
+public:
+private:
+    
+};
