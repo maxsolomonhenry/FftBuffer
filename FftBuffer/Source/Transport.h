942e320632bb57973919a5764b28b0701e8b492f
Max Henry <max.henry@mail.mcgill.ca>
2022-08-12 17:44:08 -0400

    Add transport header

---
+++ FftBuffer/Source/Transport.h
@@ -0,0 +1,42 @@
+/*
+  ==============================================================================
+
+    Transport.h
+    Created: 12 Aug 2022 3:11:38pm
+    Author:  Max Henry
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+struct Transport
+{
+public:
+    void prepare(double sampleRate, int maxBlockSize)
+    {
+        samplesPerMinute = sampleRate * 60.0;
+        ppqPositions.resize(maxBlockSize);
+    }
+    
+    void process(juce::AudioPlayHead* playHead, int numSamples)
+    {
+        if (playHead != nullptr)
+        {
+            auto info = playHead->getPosition();
+            
+            double beatsPerSample = *info->getBpm() / samplesPerMinute;
+            
+            for (int n = 0; n < numSamples; ++n)
+            {
+                ppqPositions[n] = *info->getPpqPosition() + n * beatsPerSample;
+            }
+        }
+    }
+    
+private:
+    std::vector<double> ppqPositions;
+    double samplesPerMinute;
+};
